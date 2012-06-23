a6eebe088ce0f6507c125c8f027764f77d33a5ad
yizha <ding.rickcat@gmail.com>
2012-06-23 19:40:58 +0800

    changes

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.configure('development', function(){
 
 // Routes
 app.get('/', routes.index);
-app.get('/json/movies', routes.movies);
+app.get('/json/movie_list', routes.movies);
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
